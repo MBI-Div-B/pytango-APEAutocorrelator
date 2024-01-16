d7fe0c167b9b9f857a7c78f2c46ea32bda8e5c1f
Leon <leonwww11@gmail.com>
2024-01-16 14:53:27 +0100

    addes support functions

--- APEAutocorrelator.py
+++ APEAutocorrelator.py
@@ -16,6 +16,25 @@ class resEnum(IntEnum):
     _8 = 3
     _16 = 4
 
+class fitEnum(IntEnum):
+    OFF = 0
+    GAUSSIAN = 1
+    SECH2 = 2
+    LORENTZ = 3
+
+class scanEnum(IntEnum):
+    _0 = 0
+    _150fs = 150
+    _500fs = 500
+    _1500fs = 1500
+    _15ps = 15000
+    _150ps = 150000
+
+class sensEnum(IntEnum):
+    _1 = 1
+    _10 = 10
+    _100 = 100
+
 
 class APEAutocorrelator(Device):
     # device properties
@@ -40,7 +59,113 @@ class APEAutocorrelator(Device):
         dtype= resEnum,
         access= AttrWriteType.READ_WRITE
     )
-    
+    fitType = attribute(
+        label= 'set the type of curve fit to apply to measured ACF',
+        dtype= fitEnum,
+        access= AttrWriteType.READ_WRITE
+    )
+    measStatus = attribute(
+        label= 'Measurement running',
+        dtype= "DevBoolean",
+        access= AttrWriteType.READ
+    )
+    filtering = attribute(
+        label= 'Set status of ACF filtering',
+        dtype= "DevBoolean",
+        access= AttrWriteType.READ_WRITE
+    )
+    scanRange = attribute(
+        label = 'Set scan range',
+        dtype = scanEnum,
+        unit = fs,
+        access = AttrWriteType.READ_WRITE
+    )
+    gain = attribute(
+        label = 'Set gain value',
+        dtype = 'DevLong',
+        min_value = 300,
+        max_value = 1000,
+        access = AttrWriteType.READ_WRITE
+    )
+    autoGain = attribute(
+        label = 'Activate automatic gain',
+        dtype = 'DevBoolean',
+        access = AttrWriteType.READ_WRITE
+    )
+    sensitivity = attribute(
+        label = 'set sensitivity',
+        dtype = sensEnum,
+        access = AttrWriteType.READ_WRITE
+    )
+    trigLvl = attribute(
+        label = 'Trigger level',
+        dtype = 'DevLong',
+        unit = 'mV',
+        access = AttrWriteType.READ
+    )
+    trigDly = attribute(
+        label = 'Trigger delay',
+        dtype = 'DevLong',
+        unit = 'us',
+        access = AttrWriteType.READ
+    )
+    trigFrq = attribute(
+        label = 'Trigger frequenzy',
+        dtype = 'DevLong',
+        unit = 'Hz',
+        access = AttrWriteType.READ
+    )
+    trigImp = attribute(
+        label = 'Trigger impeadence',
+        dtype = 'DevLong',
+        unit = 'Ohms',
+        access = AttrWriteType.READ
+    )
+    rawData = attribute(
+        label = 'Raw Data',
+        dtype = 'DevVarDoubleArray',
+        access = AttrWriteType.READ
+    )
+    rawData = attribute(
+        label = 'Raw Data',
+        dtype = 'DevVarDoubleArray',
+        access = AttrWriteType.READ
+    )
+    ### more Data attributes to be added
+
+    shutterFix = attribute(
+        label = 'Set fix-shutter poition',
+        dtype = 'DevBoolean',
+        access = AttrWriteType.READ_WRITE
+    )
+    shutterScan = attribute(
+        label = 'Set scan-shutter poition',
+        dtype = 'DevBoolean',
+        access = AttrWriteType.READ_WRITE
+    )
+    tune = attribute(
+        label = 'Set crystal tuning poition',
+        dtype = 'DevLong',
+        min_value = 500,
+        max_value = 11000,
+        access = AttrWriteType.READ_WRITE
+    )
+    lambdaTune = attribute(
+        label = 'Set crystal tuning i nm',
+        dtype = 'DevLong',
+        unit = 'nm',
+        access = AttrWriteType.READ_WRITE
+    )
+    crystalMove = attribute(
+        label = 'Status of crystal motor',
+        dtype = 'DevBoolean',
+        access = AttrWriteType.READ
+    )
+    crystalType = attribute(
+        label = 'Crystal type',
+        dtype = 'DevString',
+        access = AttrWriteType.READ
+    )
 
 
     def init_device(self):
--- APEAutocorrelatorHandler.py
+++ APEAutocorrelatorHandler.py
@@ -4,4 +4,69 @@ class APEAutocorrelatorHandler(object):
     def __init__(self, host, port, name):
         dev = ape_device.ape_device(host, port, name)
 
-    
+    def get_avg(self):
+        return int(ape_device.query("AVERAGE?"))
+    def set_avg(self, num):
+        ape_device.send("AVERAGE "+str(num))
+    def get_res(self):
+        return int(ape_device.query("RESOLUTION?"))
+    def set_res(self, num):
+        ape_device.send("RESOLUTION "+str(num))  
+    def get_fit(self):
+        return int(ape_device.query("FITTYPE?"))
+    def set_fit(self, num):
+        ape_device.send("FITTYPE "+str(num)) 
+    def get_running(self):
+        return bool(ape_device.query("START?"))
+    def get_filt(self):
+        return int(ape_device.query("FITTYPE?"))
+    def set_filt(self, num):
+        ape_device.send("FITTYPE "+str(int(num)))
+    def get_scanrange(self):
+        return int(ape_device.query("SCANRANGE?"))
+    def set_scanrange(self, num):
+        ape_device.send("SCANRANGE "+str(int(num)))
+    def get_gain(self):
+        return int(ape_device.query("GAIN?"))
+    def set_gain(self, num):
+        ape_device.send("GAIN "+str(int(num)))
+    def get_autoGain(self):
+        return int(ape_device.query("AUTOGAIN?"))
+    def set_autoGain(self, num):
+        ape_device.send("AUTOGAIN "+str(int(num)))
+    def get_sensitivity(self):
+        return int(ape_device.query("SENSITIVITY?"))
+    def set_sensitivity(self, num):
+        ape_device.send("SENSITIVITY "+str(int(num)))
+    def get_trigLvl(self):
+        return int(ape_device.query("LEVEL?"))
+    def get_trigDly(self):
+        return int(ape_device.query("DELAY?"))
+    def get_trigFrq(self):
+        return int(ape_device.query("FREQUENCY?"))
+    def get_trigImp(self):
+        return int(ape_device.query("IMP?"))
+    def get_rawData(self):
+        return ape_device.query("DATA?")
+    ### not usre what data format to exlpect and how to extract
+    ### to be done when device in avalablie 
+    def get_shutterFix(self):
+        return int(ape_device.query("FIX?"))
+    def set_shutterFix(self, num):
+        ape_device.send("FIX "+str(int(num)))
+    def get_shutterScan(self):
+        return int(ape_device.query("SCAN?"))
+    def set_shutterScan(self, num):
+        ape_device.send("SCAN "+str(int(num)))
+    def get_tuning(self):
+        return int(ape_device.query("TUNING?"))
+    def set_tuning(self, num):
+        ape_device.send("TUNING "+str(int(num)))
+    def get_labdaTuning(self):
+        return int(ape_device.query("LTU?"))
+    def set_labdaTuning(self, num):
+        ape_device.send("LTU "+str(int(num)))
+    def get_crysMove(self):
+        return int(ape_device.query("MOV?"))
+    def get_crysType(self):
+        return ape_device.query("SETXTAL?")
