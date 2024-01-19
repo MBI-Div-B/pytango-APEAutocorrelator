d076c79a79fa581688cbd90f8a36b85031a7c45c
Leon <leonwww11@gmail.com>
2024-01-19 11:51:47 +0100

    :

--- APEAutocorrelatorHandler.py
+++ APEAutocorrelatorHandler.py
@@ -13,64 +13,64 @@ class APEAutocorrelatorHandler(object):
     def set_avg(self, num):
         self.dev.send(":STATUS:AVERAGE "+str(num))
     def get_res(self):
-        return int(self.dev.query("RESOLUTION?"))
+        return int(self.dev.query(":STATUS:RESOLUTION?"))
     def set_res(self, num):
-        self.dev.send("RESOLUTION "+str(num))  
+        self.dev.send(":STATUS:RESOLUTION "+str(num))  
     def get_fit(self):
         return int(self.dev.query("FITTYPE?"))
     def set_fit(self, num):
-        self.dev.send("FITTYPE "+str(num)) 
+        self.dev.send(":STATUS:FITTYPE "+str(num)) 
     def get_running(self):
-        return bool(self.dev.query("START?"))
+        return bool(self.dev.query(":STATUS:START?"))
     def get_filt(self):
-        return int(self.dev.query("FITTYPE?"))
+        return int(self.dev.query(":STATUS:FILTER?"))
     def set_filt(self, num):
-        self.dev.send("FITTYPE "+str(int(num)))
+        self.dev.send(":STATUS:FILTER "+str(int(num)))
     def get_scan(self):
-        return int(self.dev.query("SCANRANGE?"))
+        return int(self.dev.query(":MOTOR:SCANRANGE?"))
     def set_scan(self, num):
-        self.dev.send("SCANRANGE "+str(int(num)))
+        self.dev.send(":MOTOR:SCANRANGE "+str(int(num)))
     def get_gain(self):
-        return int(self.dev.query("GAIN?"))
+        return int(self.dev.query(":DETECTOR:GAIN?"))
     def set_gain(self, num):
-        self.dev.send("GAIN "+str(int(num)))
+        self.dev.send(":DETECTOR:GAIN "+str(int(num)))
     def get_autoGain(self):
-        return int(self.dev.query("AUTOGAIN?"))
+        return int(self.dev.query(":DETECTOR:AUTOGAIN?"))
     def set_autoGain(self, num):
-        self.dev.send("AUTOGAIN "+str(int(num)))
+        self.dev.send(":DETECTOR:AUTOGAIN "+str(int(num)))
     def get_sensitivity(self):
-        return int(self.dev.query("SENSITIVITY?"))
+        return int(self.dev.query(":DETECTOR:SENSITIVITY?"))
     def set_sensitivity(self, num):
-        self.dev.send("SENSITIVITY "+str(int(num)))
+        self.dev.send(":DETECTOR:SENSITIVITY "+str(int(num)))
     def get_trigLvl(self):
-        return int(self.dev.query("LEVEL?"))
+        return int(self.dev.query(":TRIGGER:LEVEL?"))
     def get_trigDly(self):
-        return int(self.dev.query("DELAY?"))
+        return int(self.dev.query(":TRIGGER:DELAY?"))
     def get_trigFrq(self):
-        return int(self.dev.query("FREQUENCY?"))
+        return int(self.dev.query(":TRIGGER:FREQUENCY?"))
     def get_trigImp(self):
-        return int(self.dev.query("IMP?"))
+        return int(self.dev.query(":TRIGGER:IMP?"))
     def get_rawData(self):
-        return self.dev.query("DATA?")
+        return self.dev.query(":ACF:DATA?")
     ### not usre what data format to exlpect and how to extract
     ### to be done when device in avalablie 
     def get_shutterFix(self):
-        return int(self.dev.query("FIX?"))
+        return int(self.dev.query(":SHUTTER:FIX?"))
     def set_shutterFix(self, num):
-        self.dev.send("FIX "+str(int(num)))
+        self.dev.send(":SHUTTER:FIX "+str(int(num)))
     def get_shutterScan(self):
-        return int(self.dev.query("SCAN?"))
+        return int(self.dev.query(":SHUTTER:SCAN?"))
     def set_shutterScan(self, num):
-        self.dev.send("SCAN "+str(int(num)))
+        self.dev.send(":SHUTTER:SCAN "+str(int(num)))
     def get_tuning(self):
-        return int(self.dev.query("TUNING?"))
+        return int(self.dev.query(":XTAL:TUNING?"))
     def set_tuning(self, num):
-        self.dev.send("TUNING "+str(int(num)))
+        self.dev.send(":XTAL:TUNING "+str(int(num)))
     def get_labdaTuning(self):
-        return int(self.dev.query("LTU?"))
+        return int(self.dev.query(":XTAL:LTU?"))
     def set_labdaTuning(self, num):
-        self.dev.send("LTU "+str(int(num)))
+        self.dev.send(":XTAL:LTU "+str(int(num)))
     def get_crysMove(self):
-        return int(self.dev.query("MOV?"))
+        return int(self.dev.query(":XTAL:MOV?"))
     def get_crysType(self):
-        return self.dev.query("SETXTAL?")
+        return self.dev.query(":XTAL:SETXTAL?")
